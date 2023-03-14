c03a00a0e73267e51bc9aeaaa5e4e5fe10630cb1
brian tarqui <tarquibrian@gmail.com>
2023-03-14 14:33:17 -0400

    initializating project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Router, Link } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">hola</div>;
+}
+
+export default App;
